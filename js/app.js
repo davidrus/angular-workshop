1248c5d61179414dd6542e752b1e27204681f563
David Rus <david.rus@me.com>
2014-04-10 13:02:54 +0200

    generovatelny titulek
    Conflicts:
    	js/app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ var app = angular.module("app",['ngRoute','ngAnimate']);
 var API = "http://angular-workshop-server.herokuapp.com";
 
 app.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider){
-	$routeProvider.when('/persons',{templateUrl:'partials/persons.html'});
-	$routeProvider.when('/persons/add',{templateUrl:'partials/edit.html'});
-	$routeProvider.when('/persons/edit/:id',{templateUrl:'partials/edit.html'});
-	$routeProvider.when('/persons/:id',{templateUrl:'partials/personDetail.html'});
-	$routeProvider.when('/contact',{templateUrl:'partials/contact.html'});
+	$routeProvider.when('/persons',{templateUrl:'partials/persons.html', title:"Výpis osob"});
+	$routeProvider.when('/persons/add',{templateUrl:'partials/edit.html', title:"Přidání osoby"});
+	$routeProvider.when('/persons/edit/:id',{templateUrl:'partials/edit.html', title:"Editace osoby"});
+	$routeProvider.when('/persons/:id',{templateUrl:'partials/personDetail.html', title:"Detail osoby"});
+	$routeProvider.when('/contact',{templateUrl:'partials/contact.html', title:"Kontaktní informace"});
 	$routeProvider.otherwise({redirectTo:'/persons'});
 }]);
