aacf2cbff7bcb0e196e15f413eff4fe5edd492ad
David Rus <david.rus@me.com>
2014-05-06 16:31:36 +0200

    ukazka service

--- js/services.js
+++ js/services.js
@@ -1,9 +1,7 @@
 app.service('initCounter',[function () {
     var inits = 0;
-    return {
-        init: function(controller) {
-            inits++;
-            console.log("Init: ", controller, " init summary: ", inits);
-        }
+    this.init = function(controller) {
+        inits++;
+        console.log("Init: ", controller, " init summary: ", inits);
     };
 }]);
